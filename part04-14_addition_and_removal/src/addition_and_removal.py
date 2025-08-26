3041e995641cea19a0a9331610ddaaf8dc7608b4
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-27 00:23:06 +0200

    solutions

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,20 @@
 # Write your solution here
+list = []
+
+while True:
+    print(f"The list is now {list}")
+    choice = input("a(d)d, (r)emove or e(x)it: ").strip().lower()
+
+    if choice == "x":
+        break
+
+    if choice == "d":
+        if list == []:
+            list.append(1) #initialize the list.
+        else:
+            list.append(list[-1] + 1)
+    else:
+        list.remove(list[-1])
+    
+
+print("Bye!")
