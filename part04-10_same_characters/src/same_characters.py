3c76c7e988b064ba2fae4a36b5e0e19f5e69a18f
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-26 22:59:58 +0200

    solutions

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,5 +1,9 @@
 # Write your solution here
+def same_chars(text,index1,index2):
+    if index1 >= len(text) or index2 >= len(text):
+        return False
+    return text[index1] == text[index2]
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    print(same_chars("coder", 1, 2))
+    print(same_chars("abc", 0, 3))
