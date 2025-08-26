3cdbfb9fdd377fd1dbe8e60a34d16d429b0dc600
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-26 22:13:54 +0200

    solutions

--- part04-06_triangle/src/triangle.py
+++ part04-06_triangle/src/triangle.py
@@ -1,8 +1,15 @@
 # Copy here code of line function from previous exercise
+def line(num, string):
+    if string == '':
+        string = '*'
+    else:
+        string = string[0]
+    print(string*num)
 
 def triangle(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    for i in range(1, size + 1):
+        line(i, "#")
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
--- part04-07_shape/src/shape.py
+++ part04-07_shape/src/shape.py
@@ -1,4 +1,17 @@
 # Copy here code of line function from previous exercise and use it in your solution
 # You can test your function by calling it within the following block
+def line(num, string):
+    if string == '':
+        string = '*'
+    else:
+        string = string[0]
+    print(string*num)
+
+def shape(size1,char1,size2,char2):
+    for i in range(1, size1 + 1):
+        line(i,char1)
+    for i in range(size2):
+        line(size1,char2)
+
 if __name__ == "__main__":
     shape(5, "x", 2, "o")
