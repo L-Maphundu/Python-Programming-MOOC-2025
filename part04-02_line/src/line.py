7996eedb2af6f2335a2b967c5b7bcc5e31196c58
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-26 21:57:10 +0200

    solutions

--- part04-02_line/src/line.py
+++ part04-02_line/src/line.py
@@ -6,8 +6,6 @@ def line(num, string):
         string = string[0]
     print(string*num)
 
-
-
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     line(5, "x")
--- part04-05_square/src/square.py
+++ part04-05_square/src/square.py
@@ -1,8 +1,15 @@
 # Copy here code of line function from previous exercise
+def line(num, string):
+    if string == '':
+        string = '*'
+    else:
+        string = string[0]
+    print(string*num)
 
 def square(size, character):
     # You should call function line here with proper parameters
-    line(4, "x")
+    for i in range(size):
+        line(size, character)
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
