e0c6d392c9cd22a972a9940cafeb31b7e2e2971a
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-26 22:34:52 +0200

    solutions

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,17 @@
 # Write your solution here
+def spruce(size):
+    print("a spruce!")
+    j = 1
+    last = size 
+
+    for i in range(size): #prints a triangle
+        print(" "*(size - 1), "*"*j, sep="")
+        j += 2
+        size -= 1
+
+    print(" "*(last - 1) + "*")
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     spruce(5)
