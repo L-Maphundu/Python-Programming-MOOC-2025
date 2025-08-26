5b0e675fe769873f199f24a6fb13e9eed57a8bf6
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-26 23:29:35 +0200

    solutions

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,12 @@
 # Write your solution here
+#"You can assume all given index values will fall within your list."
+
+list = [x for x in range(1,6)]
+while True:
+    index = int(input("Index: ").strip())
+    
+    if index == -1:
+        break
+    new_value = int(input("New value: ").strip())
+    list[index] = new_value
+    print(list)
