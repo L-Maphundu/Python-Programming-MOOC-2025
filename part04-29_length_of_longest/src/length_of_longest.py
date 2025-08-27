05cdafbce5fed8854c30f70f5dbe0fcb8ae5d701
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-27 12:06:51 +0200

    solutions

--- part04-29_length_of_longest/src/length_of_longest.py
+++ part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,10 @@
 # Write your solution here
+def length_of_longest(list):
+    longest = 0
+
+    for word in list:
+        if len(word) > longest:
+            longest = len(word)
+    return longest
+
+
