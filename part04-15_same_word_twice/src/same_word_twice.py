594b6ee155a2b11616a61e754481d6c0dfd27d46
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-27 10:46:37 +0200

    solutions

--- part04-15_same_word_twice/src/same_word_twice.py
+++ part04-15_same_word_twice/src/same_word_twice.py
@@ -1,1 +1,12 @@
 # Write your solution here
+words = []
+while True:
+    word = input("Word: ").strip()
+
+    if word in words:
+        break
+
+    if word != "":
+        words.append(word)
+        
+print(f"You typed in {len(words)} different words")
--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,12 @@
 # Write your solution here
+list = []
+while True:
+    num = int(input("New item: ").strip())
+
+    if num == 0:
+        print("Bye!")
+        break
+    list.append(num)
+    print("The list now:", list)
+    print("The list in order:",sorted(list))
+
