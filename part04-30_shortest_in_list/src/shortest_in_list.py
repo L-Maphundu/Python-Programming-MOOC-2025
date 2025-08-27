06f14af3d1138705f2993a26cc047becc987003d
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-27 12:13:02 +0200

    solutions

--- part04-30_shortest_in_list/src/shortest_in_list.py
+++ part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,9 @@
 # Write your solution here
+
+def shortest(list):
+    shortest = list[0]
+
+    for word in list:
+        if len(word) < len(shortest):
+            shortest = word
+    return shortest
