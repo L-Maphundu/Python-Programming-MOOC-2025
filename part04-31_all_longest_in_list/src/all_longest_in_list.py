21a6ad31982ab6432eccf617db25a9a1d7376bd0
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-27 13:14:35 +0200

    solutions

--- part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,19 @@
 # Write your solution here
+def all_the_longest(list):
+    longest = ""
+    long = []
+    for word in list:
+        if len(word) >= len(longest):
+            if len(word) > len(longest):
+                long = []
+                longest = word
+            long.append(word)
+
+    return long
+    
+if __name__ == "__main__":
+    my_list = ['Alan', 'Steve', 'Seymour', 'Kim', 'Susan']
+    print()
+    result = all_the_longest(my_list)
+    print(result) # ['dorothy', 'richard']
+    print()
--- part04-32_integers_to_strings/src/integers_to_strings.py
+++ part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,6 @@
 # Write your solution here
+def formatted(list):
+    result = []
+    for num in list:
+        result.append(f"{num:.2f}")
+    return result
