9c42ff85964e442b1a08d9b95e284852b6a8eabb
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-29 10:23:47 +0200

    solutions

--- part04-38_grade_statistics/src/grade_statistics.py
+++ part04-38_grade_statistics/src/grade_statistics.py
@@ -1,20 +1,25 @@
 # Write your solution here
 def grade(exam,overall):
+    """Takes in two list, exam scores (exam)
+    and overall scores (overall) and returns a list of grades 
+    distribtution of the course."""
+    
+    #Stores the grades where index 0 correspends to grade 0 (fail) and index 5 is grade 5.
     distribution = [0, 0, 0, 0, 0, 0]
 
     for i in range(len(exam)):
         if exam[i] < 10 or overall[i] < 15:
-            index = 5
-        elif overall[i] < 18:
-            index = 4
-        elif overall[i] < 21:
-            index = 3
-        elif overall[i] < 24:
-            index = 2
-        elif overall[i] < 28:
-            index = 1
-        else:
             index = 0
+        elif overall[i] < 18:
+            index = 1
+        elif overall[i] < 21:
+            index = 2
+        elif overall[i] < 24:
+            index = 3
+        elif overall[i] < 28:
+            index = 4
+        else:
+            index = 5
         distribution[index] += 1
     
     return distribution
@@ -33,13 +38,13 @@ def main():
         overall.append(int(scores[0]) + int(scores[1])//10)
 
     stats = (grade(exams, overall))
-    pass_rate = (sum(stats[:5])/sum(stats))*100
+    pass_rate = (sum(stats[1:])/sum(stats))*100
 
     print("Statistics:")
     print(f"Points average: {sum(overall)/len(overall)} ")
     print(f"Pass percentage: {pass_rate:.1f}")
     print("Grade distribution: ")
-    for i in range(6):
-        print(" "*2, f"{5-i}: ", "*"*stats[i], sep="")
+    for i in range(5,-1,-1):
+        print(" "*2, f"{i}: ", "*"*stats[i], sep="")
 
 main()
