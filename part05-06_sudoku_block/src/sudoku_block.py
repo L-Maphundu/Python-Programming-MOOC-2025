17d82932c5ba138bea176692fd40dcf6a1ff7f85
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-29 20:29:03 +0200

    solutions

--- part05-06_sudoku_block/src/sudoku_block.py
+++ part05-06_sudoku_block/src/sudoku_block.py
@@ -1,1 +1,35 @@
 # Write your solution here
+def block_correct(sudoku: list, row_no: int, column_no: int):
+    """Check if a number from 1-9 is not repeated in a 3x3 matrix
+    taken from a bigger nxn matrix"""
+
+    r = row_no
+    c = column_no
+    numbers = []
+    entries = []
+
+    for i in range(r, r + 3):
+        entries += (sudoku[i])[c: c+3]
+    
+    for number in entries:
+        if number != 0 and number in numbers:
+            return False
+        numbers.append(number)
+
+    return True
+
+if __name__ == "__main__":
+    sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(block_correct(sudoku, 0, 0))
+    print(block_correct(sudoku, 1, 2))
