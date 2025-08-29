60d1f7a47bd49a82a2b0248ed3486058c33309be
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-29 17:58:28 +0200

    solutions

--- part05-03_go/src/go.py
+++ part05-03_go/src/go.py
@@ -1,1 +1,22 @@
 # Write your solution here
+
+def who_won(game_board: list):
+
+    player1 = 0
+    player2 = 0
+
+    for row in game_board: 
+        for cell in row:
+            if cell == 1:
+                player1 += 1
+            elif cell == 2:
+                player2 += 1
+
+    if player1 < player2:
+        return 2
+    elif player1 > player2:
+        return 1
+    return 0
+
+
+    
