73da480b986d6e7aa507dc8e4449bb0512afcdd6
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-29 18:23:56 +0200

    solutions

--- part05-04_sudoku_row/src/sudoku_row.py
+++ part05-04_sudoku_row/src/sudoku_row.py
@@ -1,1 +1,32 @@
 # Write your solution here
+def row_correct(sudoku: list, row_no: int):
+    """Checkers if the given row does not contain the same number
+    twice for numbers 1-9."""
+
+    numbers = [x for x in range(1,10)]
+    row = sudoku[row_no]
+  
+    for number in numbers:
+        count = 0
+        for entry in row:
+            if entry == number:
+                count += 1
+        if count > 1:
+            return False
+    return True
+
+if __name__ == "__main__":
+    sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(row_correct(sudoku, 0))
+    print(row_correct(sudoku, 1))
