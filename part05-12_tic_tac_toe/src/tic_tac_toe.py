e561d1372179573151002e96a3e175bcd6f62d3f
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-30 11:27:18 +0200

    Solutions

--- part05-12_tic_tac_toe/src/tic_tac_toe.py
+++ part05-12_tic_tac_toe/src/tic_tac_toe.py
@@ -1,7 +1,8 @@
 # Write your solution here
 def play_turn(game_board: list, x: int, y: int, piece: str):
     """Places the given piece at the given coordinates on the board(3x3 matrix).
-    y is the row numer and x the column number."""
+    y is the row numer and x the column number. Assumes the player
+    will always enter an integer."""
 
     if not (0<=x<=2) or not (0<=y<=2) or not (game_board[y][x] == ""):
         return False
