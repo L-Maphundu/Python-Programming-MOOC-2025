172669f94375edbcd59b431c4b5c1a22a3cedeef
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-30 15:00:52 +0200

    solutions

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def times_ten(start_index,end_index):
+
+    numbers = {}
+
+    for i in range(start_index, end_index +1):
+        numbers[i] = i*10
+    return numbers
+
+if __name__ == "__main__":
+    d = times_ten(3, 6)
+    print(d)
--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,19 @@
 # Write your solution here
+def factorials(n):
+    """Creates a dictionary of factorials for
+    1 to n."""
+
+    factorials = {}
+    j = 1
+
+    for i in range(1, n + 1):
+        j *= i
+        factorials[i] = j
+        
+    return factorials
+
+if __name__ == "__main__":
+    k = factorials(5)
+    print(k[1])
+    print(k[3])
+    print(k[5])
