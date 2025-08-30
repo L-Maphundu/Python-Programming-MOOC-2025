44fca4097d2b6e192c63c33290f6660e3040c7c6
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-30 11:24:02 +0200

    Solutions

--- part05-12_tic_tac_toe/src/tic_tac_toe.py
+++ part05-12_tic_tac_toe/src/tic_tac_toe.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def play_turn(game_board: list, x: int, y: int, piece: str):
+    """Places the given piece at the given coordinates on the board(3x3 matrix).
+    y is the row numer and x the column number."""
+
+    if not (0<=x<=2) or not (0<=y<=2) or not (game_board[y][x] == ""):
+        return False
+    
+    game_board[y][x] = piece
+    return True
+
+if __name__ == "__main__":
+    game_board = [['O', 'O', 'O'], ['O', '', ''], ['', 'X', 'X']]
+    print(play_turn(game_board, 3, 0, "X"))
+    print(game_board)
