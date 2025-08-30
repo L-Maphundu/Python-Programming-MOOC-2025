2cef0e2d94ac3090345eaf0fb01f00c31ad23e2d
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-30 10:06:35 +0200

    Solutions

--- part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
+++ part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def double_items(numbers: list):
+    copy = numbers[:]
+
+    for i in range(len(copy)):
+        copy[i] *= 2
+        
+    return copy
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5, 3, 11, -4]
+    numbers_doubled = double_items(numbers)
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)   
--- part05-10_sudoku_print_and_add/src/sudoku_print_and_add.py
+++ part05-10_sudoku_print_and_add/src/sudoku_print_and_add.py
@@ -1,1 +1,70 @@
 # Write your solution here
+def print_sudoku(sudoku: list):
+    """Prints a sudoku grid where "_" represents
+    and empty space."""
+
+    line_space = 1
+    row_space = 1
+    for row in sudoku:
+        line_space = 1
+        for cell in row:
+
+            if line_space != 9:
+                if cell == 0:
+                    print("_ ",end="")
+                else:
+                    print(f"{cell} ",end="")
+            else:
+                if cell == 0:
+                    print("_", end="")
+                else:
+                    print(f"{cell}", end="")
+            
+            if line_space == 3 or line_space == 6:
+                print(" ",end="")
+            line_space += 1
+
+        if row_space == 3 or row_space == 6:
+            print("\n") 
+        else:
+            print()
+        row_space += 1
+                
+        
+def add_number(sudoku: list, row_no: int, column_no: int, number:int):
+    sudoku[row_no][column_no] = number
+
+if __name__ == "__main__":
+    sudoku  = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 6],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    sudoku  = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 8],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    print_sudoku(sudoku)
+    add_number(sudoku, 0, 0, 2)
+    add_number(sudoku, 1, 2, 7)
+    add_number(sudoku, 5, 7, 3)
+    print()
+    print("Three numbers added:")
+    print()
+    print_sudoku(sudoku)
+ 
