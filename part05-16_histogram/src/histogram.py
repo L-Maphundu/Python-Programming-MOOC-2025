b0680f87b125f2addfc2e71eff77a8e6bf69d0fe
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-31 13:05:18 +0200

    solutions

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def histogram(string):
+    histogram = {}
+
+    for char in string:
+        if char not in histogram:
+            histogram[char] = "*"
+        else:
+            histogram[char] += "*"
+    
+    for key in histogram:
+        print(key, histogram[key])
+
+if __name__ == "__main__":
+    histogram("statistically")
