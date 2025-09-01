34debc5fbe52b07755bb0979fec009c730d80599
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-01 17:39:54 +0200

    solutions

--- part05-18_phone_book_v2/src/phone_book_v2.py
+++ part05-18_phone_book_v2/src/phone_book_v2.py
@@ -1,1 +1,29 @@
 # Write your solution here
+phonebook = {}
+
+while True:
+    option = input("command (1 search, 2 add, 3 quit): ").strip()
+
+    if option == "3":
+        print("quitting...")
+        break
+
+    elif option == "1":
+        name = input("name: ").strip().lower()
+        if name in phonebook:
+            for number in phonebook[name]:
+                print(number)
+        else:
+            print("no number")
+
+    elif option == "2":
+        name = input("name: ").strip().lower()
+        number = input("number: ").strip().lower()
+        if name in phonebook:
+            phonebook[name].append(number)        
+        else:
+            phonebook[name] = [number]
+        print("ok!")
+
+    else:
+        print("Please enter 1, 2 or 3!")
