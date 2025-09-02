7339798eec4b4047f17f3a6591caf2586635323a
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-02 17:14:46 +0200

    solutions

--- part05-24_oldest_person/src/oldest_person.py
+++ part05-24_oldest_person/src/oldest_person.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def oldest_person(people: list):
+    ages = []
+    for age in people:
+        ages.append(age[1])
+
+    for person in people:
+        if person[1] == min(ages):
+            return person[0]
+
+if __name__ == "__main__":
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [('Emily', 2014), ('Arthur', 1977), ('Ernest', 1985), ('Mary', 1953), ('Ellen', 1997)]
+    print(oldest_person(people))
