64c07d331761ad0b3ad6bec098f26470c2bdd314
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-02 17:21:49 +0200

    solutions

--- part05-25_older_people/src/older_people.py
+++ part05-25_older_people/src/older_people.py
@@ -1,1 +1,21 @@
 # Write your solution here
+def older_people(people: list, year: int):
+    oldies = []
+
+    for person in people:
+        if person[1] < year:
+            oldies.append(person[0])
+    return oldies
+
+if __name__ == "__main__":
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    older = older_people(people, 1979)
+    print(older)
+
+
+
