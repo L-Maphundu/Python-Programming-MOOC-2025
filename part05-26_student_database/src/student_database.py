4ef402e4292c19f115b15b9281262332f5badb01
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-03 13:01:00 +0200

    solutions

--- part05-26_student_database/src/student_database.py
+++ part05-26_student_database/src/student_database.py
@@ -2,12 +2,18 @@
 students = {} #The dictionary storing students' information
 
 def add_student(students: dict, name: str):
+    """Adds a student to the database."""
     if name not in students:
         students[name] = []
+    else:
+        print("The student already exists.")
 
-def add_course(students: dict, name: str, course_info: tuple):
+def add_course(students: dict, name: str, courseIn: tuple):
+    """Adds students' courses and their respective grades (courseIn). Does not add the course if the grade is 0
+    or the new grade is lower than the previous grade in case of an existing course."""
 
-        students[name].append(course_info)
+    if students[name] == []:
+        students[name].append(courseIn)   
 
 def print_student(students: dict, name: str):
 
@@ -30,6 +36,8 @@ def print_student(students: dict, name: str):
 if __name__ == "__main__":
     students = {}
     add_student(students, "Peter")
-    add_course(students, "Peter", ("Introduction to Programming", 3))
+    add_course(students, "Peter", ("Introduction to Programming", 2))
     add_course(students, "Peter", ("Advanced Course in Programming", 2))
+    add_course(students, "Peter", ("Data Structures and Algorithms", 0))
+    add_course(students, "Peter", ("Introduction to Programming", 23))
     print_student(students, "Peter")
