f9fb6a24aa1ff06dd091b66cab24784c39b5213b
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-03 15:44:06 +0200

    solutions

--- part05-26_student_database/src/student_database.py
+++ part05-26_student_database/src/student_database.py
@@ -11,10 +11,35 @@ def add_student(students: dict, name: str):
 def add_course(students: dict, name: str, courseIn: tuple):
     """Adds students' courses and their respective grades (courseIn). Does not add the course if the grade is 0
     or the new grade is lower than the previous grade in case of an existing course."""
+    courses = (students[name])[:]
+    courseNames = []
+    if courses == [] and courseIn[1] > 0:
+        students[name].append(courseIn)  
 
-    if students[name] == []:
-        students[name].append(courseIn)   
+    elif courseIn[1] > 0:
+        for course in courses:
+            courseNames.append(course[0])
 
+        if courseIn[0] in courseNames:
+            for course in courses:
+                if course[0] == courseIn[0] and courseIn[1] > course[1]:
+                    students[name].remove(course)
+                    students[name].append(courseIn)
+        else:
+            students[name].append(courseIn)
+
+def summary(students):
+    most_courses = 0
+    name = ""
+
+    for student, courses in students.items():
+        if len(courses) > most_courses:
+            name = student
+            most_courses = len(courses)
+    
+    print(f"students {len(students)}")
+    print(f"most courses completed {most_courses} {name}")
+    
 def print_student(students: dict, name: str):
 
     if name not in students:
@@ -36,8 +61,10 @@ def print_student(students: dict, name: str):
 if __name__ == "__main__":
     students = {}
     add_student(students, "Peter")
-    add_course(students, "Peter", ("Introduction to Programming", 2))
-    add_course(students, "Peter", ("Advanced Course in Programming", 2))
-    add_course(students, "Peter", ("Data Structures and Algorithms", 0))
-    add_course(students, "Peter", ("Introduction to Programming", 23))
-    print_student(students, "Peter")
+    add_student(students, "Eliza")
+    add_course(students, "Peter", ("Data Structures and Algorithms", 1))
+    add_course(students, "Peter", ("Introduction to Programming", 1))
+    add_course(students, "Peter", ("Advanced Course in Programming", 1))
+    add_course(students, "Eliza", ("Introduction to Programming", 5))
+    add_course(students, "Eliza", ("Introduction to Computer Science", 4))
+    summary(students)
