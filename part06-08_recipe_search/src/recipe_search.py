c394a3c7464900549745b9d1f36d9617568ed85f
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-08 23:43:31 +0200

    solutions

--- part06-08_recipe_search/src/recipe_search.py
+++ part06-08_recipe_search/src/recipe_search.py
@@ -11,15 +11,28 @@ def search_by_name(filename: str, word: str):
             if word.lower() in line.lower():
                 names.append(line.strip())
 
-
     return names
+
+def search_by_time(filename: str, prep_time: int):
+    #Pancakes, preparation time 15 min
+    results = []
+    recipes = []
+    with open(filename) as recipes_data:
+        for line in recipes_data:
+            line = line.strip()
+            recipes.append(line)
+            if line.isnumeric() and int(line) <= prep_time:
+                results.append(f"{recipes[-2]}, preparation time {line} min")
+    return results
 
 def main():
     word = "cake" #input("Enter food name: ")
+    prep_time = 20
     filename = "recipes1.txt" #input("Enter file name: ")
     recipe_names = search_by_name(filename, word)
+    recipe_times = search_by_time(filename, prep_time)
 
-    for name in recipe_names:
+    for name in recipe_times:
         print(name)
 
 main()
