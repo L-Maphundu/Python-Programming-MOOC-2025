585dde4e78296e4b5c028c83ffeee27b7d4b7b43
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-08 23:22:01 +0200

    solutions

--- part06-08_recipe_search/src/recipe_search.py
+++ part06-08_recipe_search/src/recipe_search.py
@@ -1,1 +1,26 @@
 # Write your solution here
+def search_by_name(filename: str, word: str):
+    """Takes in a file containing recipes and a word to search for
+    in the recipe names in the file and returns a list of recipe
+    names containing the word.
+    """
+
+    names = []
+    with open(filename) as recipes:
+        for line in recipes:
+            if word.lower() in line.lower():
+                names.append(line.strip())
+
+
+    return names
+
+def main():
+    word = "cake" #input("Enter food name: ")
+    filename = "recipes1.txt" #input("Enter file name: ")
+    recipe_names = search_by_name(filename, word)
+
+    for name in recipe_names:
+        print(name)
+
+main()
+
