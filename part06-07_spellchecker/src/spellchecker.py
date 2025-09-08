8d46161f262ba9c33a5277b3499dd6fcc23c71b6
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-08 22:20:41 +0200

    solutions

--- part06-07_spellchecker/src/spellchecker.py
+++ part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,38 @@
 # write your solution here
+def word_list():
+    """Reads the file wordlist.txt containing words 
+    and returns a list containing the words."""
+    
+    words = []
+    with open("wordlist.txt") as wordlist:
+        for word in wordlist:
+            word = word.strip()
+            words.append(word)
+    return words 
+
+def spellchecker(text: str, wordlist: list):
+    """Given a text, checks if every word is spelled correctly by comparing
+    it to the wordlist provided and returns the text with incorred words highlighted.
+    """
+    checked = ""
+    words = text.split(" ")
+    for word in words:
+        if word.lower() in wordlist:
+            word = f"{word} "
+        else:
+            word = f"*{word}* "
+
+        checked += word
+
+    return checked.strip()
+
+
+def main():
+    text = input("Write text: ").strip()
+    words = word_list()
+    spellcheck = spellchecker(text, words)
+    print(spellcheck)
+
+main()
+
+
