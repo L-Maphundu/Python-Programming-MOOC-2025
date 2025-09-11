b2130f211bc906ef3465e1ec7ceb06d01ed92829
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-12 00:01:57 +0200

    solutions

--- part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -1,1 +1,32 @@
 # Write your solution here
+def filter_solutions():
+    """Reads the solutions.csv file and creates two files.
+    'correct.csv' contains correct solutions to exercises from 'solutions.csv'
+    and 'incorrect.csv' contains the incorrect ones."""
+
+    with open("solutions.csv") as solutions, open("correct.csv", "w") as correct, open("incorrect.csv", "w") as incorrect:
+
+        #Reads each line from the solutions.
+        for line in solutions:
+            line = line.strip()
+            name, expression, result = line.split(";")
+            
+            #Perform the expression to check if it is correct by comparing to result.
+            if "+" in expression:
+                operation = expression.split("+")
+                computation = int(operation[0]) + int(operation[1])
+            else:
+                operation = expression.split("-")
+                computation = int(operation[0]) - int(operation[1])
+            
+            #Write the computation to the correct file according to whether the answer is correct or incorrct.
+            if computation == int(result):
+                correct.write(f"{line}\n")
+            else:
+                incorrect.write(f"{line}\n")
+                
+filter_solutions()
+            
+
+                
+
