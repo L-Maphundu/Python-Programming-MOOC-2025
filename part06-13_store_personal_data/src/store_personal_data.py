b52b1c45d422c0c05344aa14698b49130cfa51ed
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-12 00:39:54 +0200

    solutions

--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,7 @@
 # Write your solution here
+def store_personal_data(person: tuple):
+    with open("people.csv", "a") as staff:
+        staff.write(f"{person[0]};{str(person[1])};{person[2]}\n")
+
+if __name__ == "__main__":
+    store_personal_data(("Paul Paulson", 37, 175.5))
