8ffc16803c4777fd8d85a9e0662bef2a2c47a3b6
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-12 19:39:02 +0200

    solutions

--- part06-14_course_grading_part_4/src/course_grading_part_4.py
+++ part06-14_course_grading_part_4/src/course_grading_part_4.py
@@ -1,1 +1,81 @@
 # tee ratkaisu tänne
+def student_details(file):
+    """Reads a csv file containing student id, first name
+    and last name (in that order) and returns a dictionary
+    of id (key) and full name (value)."""
+    
+    student_details = {}
+    with open(file) as students:
+        for student in students:
+            student = student.strip()
+            id, name, surname = student.split(';')
+
+            if id == "id": #ignore the header row.
+                continue
+
+            student_details[id] = f"{name} {surname}"
+    
+    return student_details
+
+def aggregator(file):
+    """Reads a csv file containing student id and exercises completed or exam points
+    and returns a dictionary of id (key) and aggregate of exercises or aggregate exam points (value)."""
+
+    exercises = {}
+    with open(file) as students:
+        for student in students:
+            student = student.strip()
+            info = student.split(';')
+
+            id = info[0]
+            if id == "id":
+                continue
+
+            exercises_completed = info[1:]
+            for i in range(len(exercises_completed)):
+                exercises_completed[i] = int(exercises_completed[i])
+
+            exercises[id] = sum(exercises_completed)
+
+    return exercises
+
+def student_summary(details: dict, exercises: dict, exams: dict):
+    """Returns a dictionary of full name (key) and summary statistics comprised of exercises and grades."""
+    summary = {}
+    boundaries = [15, 18, 21, 24, 28]
+
+    for id, full_name in details.items():
+        exercise_tot = exercises[id]
+        exercise_points = int((exercise_tot*100/40)//10)
+        exam_points = exams[id]
+        total = exercise_points + exam_points
+        
+        grade = 0
+        for boundary in boundaries:
+            if total >= boundary:
+                grade += 1
+
+        summary[full_name] = [exercise_tot, exercise_points, exam_points, total, grade]
+
+    return summary
+        
+def main():
+    student_info = input("Student information: ")
+    exercise_data = input("Exercises completed: ")
+    exam_data = input("Exam points: ")
+
+    #populate the dictionaries needed by student_summary function.
+    details = student_details(student_info)
+    exercises = aggregator(exercise_data) 
+    exams = aggregator(exam_data)
+
+    summary = student_summary(details, exercises, exams)
+    
+    print(f"{'name':30}exec_nbr  exec_pts. exm_pts.  tot_pts.  grade")
+    for full_name, stats in summary.items():
+        print(f"{full_name:30}",end="")
+        for data in stats:
+            print(f"{str(data):10}",end="")
+        print()
+
+main()
