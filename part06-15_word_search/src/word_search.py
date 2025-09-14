1af1f98038189fce066261971d85e9374e7e5aa7
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-14 10:34:22 +0200

    solutions

--- part06-15_word_search/src/word_search.py
+++ part06-15_word_search/src/word_search.py
@@ -9,19 +9,20 @@ def word_list():
             word_list.append(word)
     return word_list
 
-def asterik(search_term: str):
+def asterik(search_term: str, words: list):
+    """Returns a list of all the """
     results = []
     search_term = search_term.lower()
 
     if search_term.endswith('*'):
         search_term = search_term[:-1]
-        for word in word_list():
+        for word in words:
             if word.startswith(search_term):
                 results.append(word)
 
     elif search_term.startswith('*'):
         search_term = search_term[1:]
-        for word in word_list():
+        for word in words:
             if word.endswith(search_term):
                 results.append(word)
 
@@ -29,11 +30,11 @@ def asterik(search_term: str):
 
 def find_words(search_term: str):
     """Returns a list of all the words in word_list()
-    that match the search term based on wildcards."""
-    results = []
+    that match the search term based on wildcards '.' and '*'."""
+    words = word_list()
 
     if '*' in search_term:
-        return asterik(search_term)
+        return asterik(search_term, words)
         
 def main():
     print(find_words("*vokes"))
