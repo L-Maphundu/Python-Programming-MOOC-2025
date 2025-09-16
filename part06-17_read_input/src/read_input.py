f05da27b0056db30dee42d68249a4db1222c7976
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-16 20:59:46 +0200

    solutions

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,20 @@
 # Write your solution here
+def read_input(prompt: str, lower_bound, upper_bound):
+    while True:
+        try:
+            num = int(input(f"{prompt}").strip())
+
+        except:
+            num = upper_bound + 1 #
+        
+        if lower_bound <= num <= upper_bound:
+            return num
+        else:
+            print("You must type in an integer")
+        
+
+def main():
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
+
+main()
