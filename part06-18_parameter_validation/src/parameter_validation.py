aa40e362fd7190ba4b00259ceff9ea0c940aeb45
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-09-17 12:50:09 +0200

    solutions

--- part06-18_parameter_validation/src/parameter_validation.py
+++ part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def new_person(name: str, age: int):
+    """Retunrs a tuple of name and age received as first
+    and second parameter respectively."""
+    
+    name = name.strip()
+    error1 = (name == "") or not ((2 <= len(name) <= 40) and (" " in name) ) #Assume for "A B" counts as two words making up a name
+    error2 = not (0 < age <= 150)
+   
+    if error1 or error2 :
+        raise ValueError("Something went wrong.")
+    else:
+        return (name, age)
+
+if __name__ == "__main__":
+    print(new_person("luyolo Maphundu", 32))
